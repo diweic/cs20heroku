2d619bdb535086759868f399a8d86ec80ba057ab
diweic <158785916+diweic@users.noreply.github.com>
2024-04-25 15:42:24 -0400

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var port = process.env.PORT || 3000;
+console.log("This goes to the console window");
+http.createServer(function (req, res) {
+    console.log("Server created - This goes to the console window");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<h2>Hello World</h2>");
+    res.write ("Success!  This app is deployed online");
+    res.end();
+}).listen(port);
