f301ffaf28c452c3a4cbee9a518f0d0fd68d929d
diweic <158785916+diweic@users.noreply.github.com>
2024-04-26 03:56:33 -0400

    Add files via upload

--- process.js
+++ process.js
@@ -3,7 +3,7 @@ var url = require('url');
 var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
     var parsedUrl = url.parse(req.url, true);
-    if (parsedUrl.pathname === "/process") {
+    if (parsedUrl.pathname == "/process") {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write("This is the process page");
         res.end();
@@ -14,3 +14,4 @@ http.createServer(function (req, res) {
         res.end();        
     }
   }).listen(port);
+  console.log("Server running at http://localhost:" + port);
