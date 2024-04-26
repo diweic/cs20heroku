3f8d138d6656d76416ce1cc2e877292ed741f64b
diweic <158785916+diweic@users.noreply.github.com>
2024-04-26 03:40:36 -0400

    Add files via upload

--- hello.js
+++ hello.js
@@ -2,9 +2,23 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 console.log("This goes to the console window");
 http.createServer(function (req, res) {
-    console.log("Server created - This goes to the console window");
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<h2>Hello World</h2>");
     res.write ("Success!  This app is deployed online");
+    res.write("<h2>Hello World</h2>");
+    res.write(`
+    <h1>Stock Ticker Form</h1>
+    <form action="/process" method="get">
+        <input type="radio" id="ticker" name="inputType" value="ticker" checked>
+        <label for="ticker">Stock Ticker Symbol</label><br>
+
+        <input type="radio" id="company" name="inputType" value="company">
+        <label for="company">Company Name</label><br><br>
+
+        <label for="inputValue">Enter the Stock Ticker Symbol or Company Name:</label><br>
+        <input type="text" id="inputValue" name="inputValue"><br><br>
+        
+        <input type="submit" value="Submit">            
+    </form>    
+    `);
     res.end();
 }).listen(port);
