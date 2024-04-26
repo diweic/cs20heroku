59203a2f1c823b637614ed17ded46b146a538fec
diweic <158785916+diweic@users.noreply.github.com>
2024-04-26 03:38:50 -0400

    Add files via upload

---
+++ process.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var port = process.env.PORT || 3000;
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    if (req.url == "/process")
+        res.write("This is the process page");
+    res.end();
+  }).listen(port);
