6815ce10573aeb7a1d2af1b8398a5584a33b86e5
diweic <158785916+diweic@users.noreply.github.com>
2024-04-27 16:28:52 -0400

    Add files via upload

--- webapp2heroku.js
+++ webapp2heroku.js
@@ -1,6 +1,10 @@
 var http = require('http');
 var url = require('url');
 var port = process.env.PORT || 3000;
+const MongoClient = require('mongodb').MongoClient;
+const mongoStr = "mongodb+srv://dwchen321:ZNMa3rWujgsimJzO@firstcluster.qwcwtxd.mongodb.net/?retryWrites=true&w=majority&appName=FirstCluster"
+const dbName = "Stock";
+const collectionName = "PublicCompanies";
 
 http.createServer(function (req, res) {
     var parsedUrl = url.parse(req.url, true);
@@ -26,15 +30,37 @@ http.createServer(function (req, res) {
         `);
         res.end();
     } else if (path == '/process') {
-        var query = parsedUrl.query; 
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write("<h1>current path is " + path + "<h1>"); 
-        res.write(`
-            <h1>Form Data</h1>
-            <p>Input Company Info Type: ${query.inputType}</p>
-            <p>Input Text Value: ${query.inputValue}</p>
-        `);
-        res.end();
+        res.write("<h1> Search Result <h1>");
+        var query = parsedUrl.query; 
+        var temp = {};
+        temp[query.inputType] = query.inputValue.trim();
+        MongoClient.connect(mongoStr, function(err, client) {
+            if (err) {
+                console.log("db connection error", err);
+                return;
+            }
+            const db = client.db(dbName);
+            const collection = db.collection(collectionName);
+            collection.find(temp).toArray(function(err, result) {
+                if (err) {
+                    console.log("erroring in finding in collection", err);
+                    client.close();
+                }
+                if (result.length > 0) {
+                    res.write("<ul>")
+                    result.forEach(function(element) {
+                        console.log(`Company name: ${element.company}, Ticker: ${element.ticker}, Share price: $${element.sharePrice}`);
+                        res.write(`<li>Company name: ${element.company}, Ticker: ${element.ticker}, Share price: $${element.sharePrice}</li>`);
+                    });
+                    res.write("</ul>")
+                } else {
+                    res.write("<p>No matching document lol</p>")
+                }
+                res.end();
+            })
+        });        
     } else {
         res.writeHead(404, {'Content-Type': 'text/html'});
         res.write('<h1>404 Not Found lmaoooo</h1>');
